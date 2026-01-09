3a900e5232758b614c07df4ab54ebaf9543ebc04
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-09 18:44:16 +0530

    Create video.py

---
+++ backend/app/routes/video.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, UploadFile, File
+import shutil
+from models.video_analyzer import analyze_video
+
+router = APIRouter()
+
+@router.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    path = f"temp_{file.filename}"
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return analyze_video(path)
