dac0e206bcd7f76131b422953802bab51ec2a5d1
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-09 18:42:41 +0530

    Create text.py

---
+++ backend/app/routes/text.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from models.text_analyzer import analyze_text
+
+router = APIRouter()
+
+class TextInput(BaseModel):
+    text: str
+
+@router.post("/analyze")
+def analyze(data: TextInput):
+    return analyze_text(data.text)
