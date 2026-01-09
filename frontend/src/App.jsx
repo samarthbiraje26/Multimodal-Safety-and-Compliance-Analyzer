94f3cf1fade372cce1ea7800a9ad113e655ba1f4
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-09 18:57:59 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Text from "./pages/Text";
+import Image from "./pages/Image";
+import Audio from "./pages/Audio";
+import Video from "./pages/Video";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/text" element={<Text />} />
+        <Route path="/image" element={<Image />} />
+        <Route path="/audio" element={<Audio />} />
+        <Route path="/video" element={<Video />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
