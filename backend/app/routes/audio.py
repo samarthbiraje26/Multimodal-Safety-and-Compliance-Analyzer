050870538735d2a700806131af637a60906e228d
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-11 16:24:36 +0530

    Update audio.py

--- backend/app/routes/audio.py
+++ backend/app/routes/audio.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, UploadFile, File
+import shutil
 from app.models.audio_analyzer import analyze_audio
 
 router = APIRouter()
 
 @router.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
-    return analyze_audio(file.filename)
+    path = f"temp_{file.filename}"
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return analyze_audio(path)
