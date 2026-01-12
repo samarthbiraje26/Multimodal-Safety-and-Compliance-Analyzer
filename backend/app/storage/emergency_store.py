2a649357d0cc0603fc14c994c028936c797783d5
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-12 20:10:02 +0530

    Delete emergency_store.py

--- backend/app/storage/emergency_store.py
+++
@@ -1,8 +0,0 @@
-emergencies = []
-
-def add_emergency(event):
-    emergencies.insert(0, event)  # latest on top
-    return event
-
-def get_all_emergencies():
-    return emergencies[:50]  # limit for dashboard
