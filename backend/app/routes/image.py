a560100b3a9e2765725077b881845a623340d674
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-13 18:54:06 +0530

    Update image.py

--- backend/app/routes/image.py
+++ backend/app/routes/image.py
@@ -1,14 +1,8 @@
 from fastapi import APIRouter, UploadFile, File
 from app.analyzers.image_analyzer import analyze_image
-from app.utils.sms_alert import send_sms
 
 router = APIRouter(prefix="/image", tags=["Image"])
 
 @router.post("/analyze")
 async def analyze_image_route(file: UploadFile = File(...)):
-    result = analyze_image(file)
-
-    if result["status"] == "DANGER":
-        send_sms(f"🚨 IMAGE EMERGENCY DETECTED: {result['label']}")
-
-    return result
+    return analyze_image(file)
