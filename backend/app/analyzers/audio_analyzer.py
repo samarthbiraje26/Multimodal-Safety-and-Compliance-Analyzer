58f70fe2dfdc66d06c34175f678c537af038386d
samarthbiraje26 <samarthbiraje@gmail.com>
2026-01-19 16:48:07 +0530

    update analyzers

--- backend/app/analyzers/audio_analyzer.py
+++ backend/app/analyzers/audio_analyzer.py
@@ -1,9 +1,4 @@
 from ..models.model_loader import audio_model
-from backend.app.models.model_loader import (
-    whisper_model,
-    whisper_processor,
-    DEVICE
-)
 from app.analyzers.text_analyzer import analyze_text
 
 def analyze_audio(path):
